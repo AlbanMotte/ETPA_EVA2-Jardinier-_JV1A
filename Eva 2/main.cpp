e1a1e08637b94f945aa8516af1925af047f47da7
Alban Motté <93644991+AlbanMotte@users.noreply.github.com>
2022-05-10 09:31:33 +0200

    Add files via upload

---
+++ Eva 2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+
+int main(){
+
+    std::cout << "Bienvenue dans ce jeu de jardinage ! Vous aller pouvoir planter des fleurs pour les revendre !" <<std::endl;
+    
+}
---
+++ Eva 2/planteCarnivore.h
@@ -0,0 +1,14 @@
+#ifndef PLANTECARNIVORE_H
+#define PLANTECARNIVORE_H
+
+#include <string>
+#include "plante.cpp"
+
+    class Carnivore : public Plante {
+
+        public :
+            Carnivore();
+
+    };
+
+#endif
